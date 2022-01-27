d9173865dd3e4be13430f2bd4b204fb1998a2e63
Titus Chang <50413143+tc1316@users.noreply.github.com>
2022-01-27 14:29:11 +0000

    Revert "Added friend requests"

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,18 +2,12 @@ class FriendshipsController < ApplicationController
   def create
     @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
     if @friendship.save
-      flash[:notice] = "Sent friend request."
+      flash[:notice] = "Added friend."
       redirect_to users_url 
     else
-      flash[:notice] = "Unable to send friend request."
+      flash[:notice] = "Unable to add friend."
       redirect_to users_url 
     end
-  end
-
-  def update
-    @friendship = Friendship.find(params[:id])
-    @friendship.update(:accepted => true)
-    redirect_to posts_url
   end
 
   def destroy
--- db/migrate/20220127105839_add_accepted_to_friendships.rb
+++
@@ -1,5 +0,0 @@
-class AddAcceptedToFriendships < ActiveRecord::Migration[6.1]
-  def change
-    add_column :friendships, :accepted, :boolean, :default => false
-  end
-end
